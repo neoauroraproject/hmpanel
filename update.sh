452f0d3077f61def7aa6d971139cd99f87d82dab
Aurora Net <hmrayserver@gmail.com>
2026-07-02 01:31:16 +0400

    chore: bump version to 1.4.5

--- update.sh
+++ update.sh
@@ -199,7 +199,7 @@ END \$\$;
     warn "Health check timeout. Check logs: docker compose logs panel-app"
   else
     echo ""
-    log "HMPanel Panel successfully updated to version 1.4.4!"
+    log "HMPanel Panel successfully updated to version 1.4.5!"
   fi
 
   info "Cleaning up old images..."
